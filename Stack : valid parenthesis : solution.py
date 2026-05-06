fb1b19dad0ffb0744aa66751e27142f61458c4d0
Arun <arunnn101010@gmail.com>
2026-05-06 23:12:53 +0530

    Update Stack : valid parenthesis : solution.py

--- Stack : valid parenthesis : solution.py
+++ Stack : valid parenthesis : solution.py
@@ -1,1 +1,29 @@
+# Promblem - valid parenthesis 
+# Appraoch - Stack 
+# Time and space complexity - 0(n) & 0(n)
+# Leetcode and diffculty level - 20 & easy
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
 
+        for(int i=0; i<s.size(); i++) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') { // opening 
+                st.push(s[i]);
+            } else {
+                if(st.size() == 0) {
+                    return false;
+                }
+
+                if((st.top() == '(' && s[i] == ')') ||
+                    (st.top() == '{' && s[i] == '}') ||
+                    (st.top() == '[' && s[i] == ']')) {
+                        st.pop();
+                } else { // no match 
+                    return false;
+                }   
+            }
+        }
+        return st.size() == 0;
+    }
+};
