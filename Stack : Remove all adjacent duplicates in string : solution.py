dda8e1698052e6df9a28436f3123e414053a3156
Arun <arunnn101010@gmail.com>
2026-05-07 23:18:17 +0530

    Update Stack : Remove all adjacent duplicates in string : solution.py

--- Stack : Remove all adjacent duplicates in string : solution.py
+++ Stack : Remove all adjacent duplicates in string : solution.py
@@ -1,1 +1,19 @@
+# Promblem - remove all adjacent duplicates in string 
+# Approach - stack
+# Time and space complexity - 0(n) & 0(n)
+# Leetcode and diffculty level - 1047 & easy 
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string st = "";
 
+        for(char c : s) {
+            if(!st.empty() && st.back() == c) 
+                st.pop_back();
+        
+            else 
+                st.push_back(c);
+        }
+        return st;
+    }
+};
