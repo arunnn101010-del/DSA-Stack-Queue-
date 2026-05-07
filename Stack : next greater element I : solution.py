8f54869526faf13ea6b23feb5861bc04ad81123d
Arun <arunnn101010@gmail.com>
2026-05-07 21:26:03 +0530

    Update Stack : next greater element I : solution.py

--- Stack : next greater element I : solution.py
+++ Stack : next greater element I : solution.py
@@ -1,1 +1,31 @@
+# Promblem - next gretaer element I 
+# Appraoch - stack 
+# Time and space complexity - 0(n) & 0(n)
+# Leetcode and diffculty level - 496 & easy 
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        
+        stack<int> st;
+        unordered_map<int,int> mp;
 
+        for(int num : nums2) {
+
+            while(!st.empty() && st.top() < num) {
+                mp[st.top()] = num;
+                st.pop();
+            }
+            st.push(num);
+        }
+        vector<int> res;
+
+        for(int num : nums1) {
+
+            if(mp.count(num)) 
+                res.push_back(mp[num]);
+            else
+                res.push_back(-1);
+        }
+        return res;
+    }
+};
