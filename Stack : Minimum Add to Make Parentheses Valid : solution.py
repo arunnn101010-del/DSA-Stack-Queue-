32734718affe19bfef37dca06e039534175dad12
Arun <arunnn101010@gmail.com>
2026-05-09 00:32:47 +0530

    Update Stack : Minimum Add to Make Parentheses Valid : solution.py

--- Stack : Minimum Add to Make Parentheses Valid : solution.py
+++ Stack : Minimum Add to Make Parentheses Valid : solution.py
@@ -1,1 +1,25 @@
+# Promblem - Minimum Add to Make Parentheses Valid
+# Appraoch - stack
+# Time and space complexity - 0(n) & 0(n) 
+# Leetcode & diffculty level - 921 & easy
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        
+        stack<char> st;
+        int ans = 0;
 
+        for(char c : s) {
+            if(c == '(') {
+                st.push(c);
+            }
+            else {
+                if(!st.empty())
+                st.pop();
+                else
+                    ans++;
+            }
+        }
+        return ans + st.size();
+    }
+};
