e9501590370637fe1fd6ec889504d1a3e5a3b6ec
Arun <arunnn101010@gmail.com>
2026-05-10 00:00:09 +0530

    Add baseball game score calculation logic
    Implement the baseball game scoring system using a stack.

--- Stack : baseball game : solution.py
+++ Stack : baseball game : solution.py
@@ -1,1 +1,43 @@
+# Promblem - baseball game 
+# Appraoch - stack 
+# time and space complexity - 0(n) & 0(n)
+# leetcode and diffculty level - 682 & easy
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        
+        stack<int> st;
+
+        for(string op : ops) {
+
+            if(op == "C") {
+                st.pop();
+            }
+            else if(op == "D") {
+                st.push(st.top() * 2);
+            }
+            else if(op == "+") {
+                int top1 = st.top();
+                st.pop();
+
+                int top2 = st.top();
+
+                st.push(top1);
+
+                st.push(top1 + top2);
+            }
+            else {
+                st.push(stoi(op));
+            }
+        }
+
+        int sum = 0;
+
+        while(!st.empty()) {
+            sum += st.top();
+            st.pop();
+        }
+        return sum;
+    }
+};
 
