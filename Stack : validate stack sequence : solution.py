770b8d014beaff6c84b972ad3927062ba05b51de
Arun <arunnn101010@gmail.com>
2026-05-10 00:08:11 +0530

    Update Stack : validate stack sequence : solution.py

--- Stack : validate stack sequence : solution.py
+++ Stack : validate stack sequence : solution.py
@@ -1,1 +1,21 @@
+# Promblem - validate stack sequence 
+# appraoch - stack
+# Time and space complexity - 0(n) & 0(n) 
+# Leecode and diffculty level - 946 & easy
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        int j = 0;
 
+        for(int x : pushed) {
+            st.push(x);
+
+            while(!st.empty() && st.top() == popped[j]) {
+                st.pop();
+                j++;
+            }
+        }
+        return st.empty();
+    }
+};
