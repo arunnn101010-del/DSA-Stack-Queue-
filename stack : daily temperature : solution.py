77eddc7443ef86e9ed57af29c69c4b0abc390bf6
Arun <arunnn101010@gmail.com>
2026-05-10 20:48:45 +0530

    Update stack : daily temperature : solution.py

--- stack : daily temperature : solution.py
+++ stack : daily temperature : solution.py
@@ -1,1 +1,27 @@
+# promblem - daily temperature 
+# Approach - stack
+# Time and space complexity - 0(n) & 0(n) 
+# Leetcode and diffculty level - 739 & medium 
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n = temp.size();
 
+        vector<int> ans(n,0);
+
+        stack<int> st;
+
+        for(int i=0; i < n; i++) {
+
+            while(!st.empty() && temp[i] > temp[st.top()]) {\
+
+                int idx = st.top();
+                st.pop();
+
+                ans[idx] = i - idx;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
