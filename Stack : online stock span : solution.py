abfc354043e28012892fc2d04966a9cb6ab7bf16
Arun <arunnn101010@gmail.com>
2026-05-11 18:57:06 +0530

    Update Stack : online stock span : solution.py

--- Stack : online stock span : solution.py
+++ Stack : online stock span : solution.py
@@ -1,1 +1,22 @@
+# Promblem - online stock span 
+# Approach - stack 
+# Time and spaace complexity - 0(n) & 0(n) 
+# Leetcode and diffculty level - 901 & medium 
+class StockSpanner {
+public:
 
+    stack<pair<int,int>> st;
+
+    StockSpanner() {
+    }
+    
+    int next(int price) {
+        int span = 1;
+        while(!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+        st.push({price,span});
+        return span;
+    }
+};
